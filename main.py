a627eca167d27c0efc040b6c0c20913de9d1f20b
krishum77 <balccbf@gmail.com>
2021-08-24 12:53:50 +0200

    snake moving on the canvas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random as rand
+# import random as rand
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,35 +10,17 @@ screen.title("Snake Game")
 screen.tracer(0)
 screen.listen()
 
-turtleObj = Turtle("square")
-turtleObj.color("white")
-turtleObj.penup()
+snake = Snake()
 
-turtleObj1 = Turtle("square")
-turtleObj1.color("white")
-turtleObj1.penup()
-turtleObj1.setposition(turtleObj.xcor()-20, turtleObj.ycor())
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
-def new_segment(x_cord, y_cord):
-    new_turtle = Turtle("square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.setposition(x_cord-20, y_cord)
+start_game = True
+while start_game:
+    screen.update()
+    time.sleep(0.1)
+    snake.move_forward()
 
-
-def up():
-    pass
-def down():
-    pass
-def right():
-    pass
-def left():
-    pass
-
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
-screen.onkey(right, "Right")
-screen.onkey(left, "Left")
-
-start_game = False
 screen.exitonclick()
